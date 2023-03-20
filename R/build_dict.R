94759226a9cacee98da6ee52fe5ed04af450be4d
Thanh <thanhmle809@gmail.com>
2023-03-19 23:29:40 -0400

    rewrite header for build_dict

--- R/build_dict.R
+++ R/build_dict.R
@@ -1,4 +1,6 @@
-# build.dict: receive sequence list, create a hash table as a dictionary to store id, score, and origin
+#' @title Build a hashtable with all possible ids with empty score
+#' @param idlist list of possible id resulting from the generate_id function
+#' @return a hash table
 build.dict <- function(idlist) {
   dict <- hashtab()
   for (i in idlist) {
